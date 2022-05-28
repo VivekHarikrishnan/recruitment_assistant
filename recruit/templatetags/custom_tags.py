a88cc8081cf4d87e2917c2563fcc9d9e21095501
Vivek Harikrishnan R <r.vivekharikrishnan@gmail.com>
2022-05-29 01:09:44 +0530

    Updated skills relationship in templates

--- recruit/templatetags/custom_tags.py
+++ recruit/templatetags/custom_tags.py
@@ -1,7 +1,16 @@
 from django import template
+from recruit.models import JobSkill
 
 register = template.Library()
 
-@register.filter(name='skills_to_str')
-def convert_skills_list_to_str(skills) -> str:
+def skills_to_str(skills: list) -> str:
     return ', '.join([skill.name for skill in skills])
+
+@register.filter(name='primary_skills_str')
+def convert_primary_skills_str(skills) -> str:    
+    return ', '.join([skill.name for skill in skills.filter(jobskill__category=JobSkill.Category.PRIMARY)])
+
+
+@register.filter(name='secondary_skills_str')
+def convert_secondary_skills_str(skills) -> str:
+    return ', '.join([skill.name for skill in skills.filter(jobskill__category=JobSkill.Category.SECONDARY)])
