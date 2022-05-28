3404e7164bae053ec26098304736a3a843986260
Vivek Harikrishnan R <r.vivekharikrishnan@gmail.com>
2022-05-28 13:57:06 +0530

    Adding company & job models.
    Added models
    Registered them into admin site.

--- recruit/models.py
+++ recruit/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=75)
+    code = models.CharField(max_length=3)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+
+class Job(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    primary_skills = models.ManyToManyField(Skill, related_name="job_primary_skills")
+    secondary_skills = models.ManyToManyField(Skill, related_name="job_secondary_skills")
+    experience = models.CharField(max_length=50)
+    job_description = models.TextField()
--- recruitment_assisstant/settings.py
+++ recruitment_assisstant/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recruit.apps.RecruitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
