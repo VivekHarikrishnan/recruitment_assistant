e232fcac3d4f9b103bf0e2d6a76a1770028436cb
Vivek Harikrishnan R <r.vivekharikrishnan@gmail.com>
2022-05-28 14:40:55 +0530

    Added columns to skills & bootstrap data for country & skill

--- recruit/models.py
+++ recruit/models.py
@@ -3,15 +3,18 @@ from django.contrib.auth.models import User
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=75)
-    code = models.CharField(max_length=3)
+    name = models.CharField(max_length=75, unique=True)
+    code = models.CharField(max_length=3, unique=True)
 
     def __str__(self) -> str:
         return f"{self.name} ({self.code})"
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
+    domain = models.CharField(max_length=50, null=True)
+    category = models.CharField(max_length=50, null=True)
+    aliases = models.CharField(max_length=100, null=True)
 
     def __str__(self) -> str:
         return f"{self.name}"
